49e055fcf948f3ef3856eb9fc610fe49ec313afd
roro3164 <mornet.romain2@gmail.com>
2024-06-06 18:12:50 +0200

    cardAnnonceCovoit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AlertComponent } from './components/alert/alert.component.js';
+import { CardAnnonceCovoitComponent } from './components/card-annonce-covoit/card-annonce-covoit.component.js';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AlertComponent],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, AlertComponent, CardAnnonceCovoitComponent ],
+  templateUrl: './app.component.html', 
   styleUrl: './app.component.css'
 })
 export class AppComponent {
