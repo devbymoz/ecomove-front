a265483ca2d2b572ee3f0b547af2699cb4ca1786
devbymoz <mzaoui.2409@gmail.com>
2024-06-06 13:12:35 +0200

    couleur Tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AlertComponent } from './components/alert/alert.component.js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AlertComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ecomoveFront';
+
 }
