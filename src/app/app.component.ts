1d5bb70da51f62f7c7b55a744def9dba124cd0af
devbymoz <mzaoui.2409@gmail.com>
2024-06-07 10:46:27 +0200

    dossier cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,9 @@ import { BottomBarComponent } from './components/bottom-bar/bottom-bar.component
 import { ButtonComponent } from './components/button/button.component.js';
 import { FooterComponent } from './components/footer/footer.component.js';
 import { CardCarpoolHistoricComponent } from './components/cards/card-carpool-historic/card-carpool-historic.component.js';
-import { CardAnnonceCovoitComponent } from './components/card-annonce-covoit/card-annonce-covoit.component.js';
-import { CarCardComponent } from './components/car-card/car-card.component.js';
-import { CardReservationComponent } from './components/card-reservation/card-reservation.component.js';
+import { CardAnnonceCovoitComponent } from './components/cards/card-annonce-covoit/card-annonce-covoit.component.js';
+import { CarCardComponent } from './components/cards/car-card/car-card.component.js';
+import { CardReservationComponent } from './components/cards/card-reservation/card-reservation.component.js';
 
 @Component({
   selector: 'app-root',
