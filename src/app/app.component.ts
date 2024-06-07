d519d461676490528cf3f80a39d2ad3cd12d561e
devbymoz <mzaoui.2409@gmail.com>
2024-06-07 10:31:14 +0200

    refacto card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { AlertComponent } from './components/alert/alert.component.js';
 import { BottomBarComponent } from './components/bottom-bar/bottom-bar.component.js';
 import { ButtonComponent } from './components/button/button.component.js';
 import { FooterComponent } from './components/footer/footer.component.js';
-import { CardCarpoolHistoricComponent } from './components/card-carpool-historic/card-carpool-historic.component.js';
+import { CardCarpoolHistoricComponent } from './components/cards/card-carpool-historic/card-carpool-historic.component.js';
 import { CardAnnonceCovoitComponent } from './components/card-annonce-covoit/card-annonce-covoit.component.js';
 
 @Component({
--- src/app/components/cards/card-carpool-historic/card-carpool-historic.component.ts
+++ src/app/components/cards/card-carpool-historic/card-carpool-historic.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardBottomComponent } from '../../card-bottom/card-bottom.component.js';
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-card-carpool-historic',
   standalone: true,
-  imports: [],
+  imports: [CardBottomComponent, NgStyle],
   templateUrl: './card-carpool-historic.component.html',
   styleUrl: './card-carpool-historic.component.css'
 })
