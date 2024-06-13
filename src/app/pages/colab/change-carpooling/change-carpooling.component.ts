8c39f11d8239c138817e9fee10ff175debf1ec6d
roro3164 <mornet.romain2@gmail.com>
2024-06-13 09:46:38 +0200

    modifié une annonce

---
+++ src/app/pages/colab/change-carpooling/change-carpooling.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-carpooling',
+  standalone: true,
+  imports: [],
+  templateUrl: './change-carpooling.component.html',
+  styleUrl: './change-carpooling.component.css'
+})
+export class ChangeCarpoolingComponent {
+
+}
