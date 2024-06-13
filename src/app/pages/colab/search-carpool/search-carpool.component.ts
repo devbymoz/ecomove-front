863645d0d478b603908eae7d8d2d7a4f2499df83
Lidia <enologahernandez@gmail.com>
2024-06-13 18:06:33 +0200

    view-vehicle

--- src/app/pages/colab/search-carpool/search-carpool.component.ts
+++ src/app/pages/colab/search-carpool/search-carpool.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CardAnnonceCovoitComponent } from '../../../components/cards/card-annonce-covoit/card-annonce-covoit.component';
 import { NumberPageComponent } from '../../../components/number-page/number-page.component';
-import { TimeSelectorComponent } from '../../../components/time-selector/time-selector.component';
+import { CalendarComponent } from '../../../components/calendar/calendar.component';
+
 
 @Component({
   selector: 'app-search-carpool',
   standalone: true,
-  imports: [CardAnnonceCovoitComponent, NumberPageComponent, TimeSelectorComponent],
+  imports: [CardAnnonceCovoitComponent, NumberPageComponent, CalendarComponent],
   templateUrl: './search-carpool.component.html',
   styleUrl: './search-carpool.component.css'
 })
